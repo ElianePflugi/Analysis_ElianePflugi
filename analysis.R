215a1bbdc106f1b109ce058c0879f2c494e0216a
ElianePflugi <escpescp@gmail.com>
2021-09-15 16:33:06 +0200

    Update analysis.R
    Removing certain participants

--- analysis.R
+++ analysis.R
@@ -1,1 +1,13 @@
 #analysis.R
+library(dplyr)
+library(data.table)
+
+#adding Verhaltensdaten.txt to R
+data <- read.table ("~/Desktop/Gittest/Analysis_ElianePflugi/Verhaltensdaten.txt", sep="\t", header = TRUE)
+
+data
+count(data)
+
+#removing participants with "remove" in subject filter
+data2 <- data [!(data$subject_filter=="remove"),]
+count(data2)
