6c9bf1b232385141c2a0124ea4759141bb68a0c2
ElianePflugi <escpescp@gmail.com>
2021-09-15 17:23:06 +0200

    Update analysis.R
    correlations with word recall added

--- analysis.R
+++ analysis.R
@@ -26,5 +26,8 @@ c2 <- cor(data2$D2_KL_Tag1, data2$D2_KL_Tag2, use="pairwise")
 c2
 cor.test(data2$D2_KL_Tag1, data2$D2_KL_Tag2) 
 
+#10
+cor(data2$words_hit_IR, data2[,c("words_hit_SD", "D2_KL_Tag1", "mac_2b_s1", "iaps_hit_SD_day1", "mrt_0b_s1", "pos1_ar_mean")], use="pairwise")
+
 
 
